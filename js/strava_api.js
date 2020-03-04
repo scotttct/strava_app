0f4c8a087cf6039e689e5ce1a03500127a3b50e7
ScottTomassetti <scott_t_ct@hotmail.com>
2020-03-04 08:28:14 -0500

    Added License and env variables and necessary packages to fix issues and secure codes on GitHub

--- js/strava_api.js
+++ js/strava_api.js
@@ -1,15 +1,16 @@
+//this code was taken from Github Repository: https://github.com/fpolig01/Code_From_Tutorials/tree/master/Strava_Api
+const auth_link = "https://www.strava.com/oauth/token";
+require('dotenv').config();//setup environmental variable taken from https://www.youtube.com/watch?v=17UVejOw3zA
 
-const auth_link = "https://www.strava.com/oauth/token"
+function getActivites(res) {
 
-function getActivites(res){
-
-    const activities_link = `https://www.strava.com/api/v3/athlete/activities?access_token=${res.access_token}`
+    const activities_link = `https://www.strava.com/api/v3/athlete/activities?access_token=${res.access_token}`;
     fetch(activities_link)
-        .then((res) => console.log(res.json()))
+        .then((res) => console.log(res.json()));
 }
 
-function reAuthorize(){
-    fetch(auth_link,{
+function reAuthorize() {
+    fetch(auth_link, {
         method: 'post',
         headers: {
             'Accept': 'application/json, text/plain, */*',
@@ -18,15 +19,15 @@ function reAuthorize(){
         },
 
         body: JSON.stringify({
-            // token_type: 'bearer',
-            client_id: '44242',
-            client_secret: '469a81b5bfb679b8576db8607d2054d3c3b698eb',
-            refresh_token: '5ac5800179163d8735503431c9139ff962fa08f7',
+
+            client_id: 'STRAVA_USER',
+            client_secret: 'STRAVA_API_SECRET',
+            refresh_token: 'STRAVA_API_REFRESH_TOKEN',
             grant_type: 'refresh_token'
         })
     })
-    .then(res => getActivites(res))
-      
+        .then(res => getActivites(res));
+
 }
 
-reAuthorize()
+reAuthorize();
---
+++ py/strava_api.py
@@ -0,0 +1,26 @@
+import requests
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+auth_url = "https://www.strava.com/oauth/token"
+activites_url = "https://www.strava.com/api/v3/athlete/activities"
+
+payload = {
+    'client_id': "44242",
+    'client_secret': '469a81b5bfb679b8576db8607d2054d3c3b698eb',
+    'refresh_token': '5ac5800179163d8735503431c9139ff962fa08f7',
+    'grant_type': "refresh_token",
+    'f': 'json'
+}
+
+print("Requesting Token...\n")
+res = requests.post(auth_url, data=payload, verify=False)
+access_token = res.json()['access_token']
+print("Access Token = {}\n".format(access_token))
+
+header = {'Authorization': 'Bearer ' + access_token}
+param = {'per_page': 200, 'page': 1}
+my_dataset = requests.get(activites_url, headers=header, params=param).json()
+
+print(my_dataset[0]["name"])
+print(my_dataset[0]["map"]["summary_polyline"])
